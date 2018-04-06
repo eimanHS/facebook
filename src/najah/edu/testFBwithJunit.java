83ebbceeb5c6aac19f1d55f810cdb19429b49527
acer <acer@10.0.0.27>
2018-04-06 10:41:23 +0300

    it worked just now

--- src/najah/edu/testFBwithJunit.java
+++ src/najah/edu/testFBwithJunit.java
@@ -1,29 +1,31 @@
 package najah.edu;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
-
-class testFBwithJunit {
+public class testFBwithJunit {
 static WebDriver driver;
 
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@BeforeClass
+public static void setUp() throws Exception {
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\acer\\Desktop\\3rdY2ndSYM\\softwear\\jars for projects\\geckodriver.exe");
 		driver = new FirefoxDriver();
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@AfterClass
+	public static void  tearDown() throws Exception {
 	}
 
 	@Test
-	void test() {
+	public void test() {
 		driver.get("https://ar-ar.facebook.com/");
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("raya.mah.987@gmail.com");
